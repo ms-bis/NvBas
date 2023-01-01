732e107b4046215a219289adac869217077995d3
msbis <87420375+ms-bis@users.noreply.github.com>
2023-01-01 17:07:54 +0600

    Update astro.lua

--- astro.lua
+++ astro.lua
@@ -212,7 +212,7 @@ local config = {
                         ["<leader>T"] = { "<cmd>TransparentToggle<cr>", desc = "Transparent Toggle" },
                         ["<leader>tr"] = { "<cmd>RnvimrToggle<cr>", desc = "Ranger Toggle" },
                         ["<leader>uU"] = { "<cmd>CursorWordToggle<cr>", desc = "cursorline toggle" },
-                        ["<leader>uP"] = { "<cmd>TSPlaygroundToggle<cr>", desc = "preview" },
+                        ["<leader>uA"] = { "<cmd>AerialToggle<cr>", desc = "Aerial preview" },
                         -- quick save
                         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
                 },
@@ -265,31 +265,6 @@ local config = {
                         },
                         {
                                 "kevinhwang91/rnvimr",
-                        },
-                        {
-                                "nvim-treesitter/playground",
-                                config = function()
-                                        require("nvim-treesitter.configs").setup {
-                                                playground = {
-                                                        enable = true,
-                                                        disable = {},
-                                                        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-                                                        persist_queries = false, -- Whether the query persists across vim sessions
-                                                        keybindings = {
-                                                                toggle_query_editor = "o",
-                                                                toggle_hl_groups = "i",
-                                                                toggle_injected_languages = "t",
-                                                                toggle_anonymous_nodes = "a",
-                                                                toggle_language_display = "I",
-                                                                focus_language = "f",
-                                                                unfocus_language = "F",
-                                                                update = "R",
-                                                                goto_node = "<cr>",
-                                                                show_help = "?",
-                                                        },
-                                                },
-                                        }
-                                end,
                         },
                         {
                                 "ibhagwan/fzf-lua",
@@ -423,4 +398,4 @@ local config = {
         end,
 }
 
-return config 
+return config
